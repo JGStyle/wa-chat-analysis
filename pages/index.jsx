d8fb9c03d98c8729eb4d43db1497e86b7f1e176d
JGStyle <jgs@jws.de>
2021-05-30 01:06:00 +0200

    added check mark, input properly parsed, added progressbar

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,51 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Text, Button, Spacer } from "@geist-ui/react";
+import { Text, Spacer, Progress } from "@geist-ui/react";
+import { CheckInCircleFill } from "@geist-ui/react-icons"
+import { useEffect, useState } from "react";
 import { Doughnut } from "react-chartjs-2";
 import styles from "../styles/Home.module.css";
-import { data } from "browserslist";
+const whatsapp = require("whatsapp-chat-parser")
 
 export default function Home() {
+  const [progress, setProgress] = useState(0)
+  const [analyseProgress, setAnalyseProgress] = useState(0)
+
+  function processData(content) {
+    whatsapp.parseString(content)
+    .then(messages => {
+      setAnalyseProgress(100)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  useEffect(() => {
+    const fileSelector = document.getElementById('wafile')
+    fileSelector.addEventListener('change', (event) => {
+      const file = event.target.files
+      try {
+        readFile(file[0])
+      } catch(err) {
+        console.log(err)
+      } 
+    })
+  }, [])
+
+  function readFile(path) {
+    const reader = new FileReader()
+    reader.addEventListener('load', (event) => {
+      let res = event.target.result
+      setProgress(100)
+      processData(res)
+    })
+    reader.addEventListener('progress', (event) => {
+      if (event.loaded && event.total) {
+        setProgress(event.loaded/event.total *100)
+      }
+    });
+    reader.readAsText(path)
+    }
+
   const data = {
     labels: ["Alisia", "Lukas", "Josef"],
     datasets: [
@@ -21,21 +61,32 @@ export default function Home() {
   };
   return (
     <div className={styles.maincontainer}>
-      <Text h1>Whatsapp chat group analysis</Text>
+      <Text h1>Whatsapp group chat analysis</Text>
       <Text h3>Upload your group chat file here</Text>
+      <Text h3>Analysis will start automatically once you select the file.</Text>
       <Spacer y={1.5} />
-      <input type="file" name="wafile" id="wafile" />
+
+      <label for="wafile" className={styles.filelabel}>Choose your file →</label>
+      <input type="file" name="wafile" id="wafile" size="80" className={styles.fileinput}/>
+
       <Spacer y={0.5} />
+
+      <Text h3>Read file {progress > 99 &&
+      <CheckInCircleFill/>
+      } </Text>
+      
+      <Progress value={progress} />
+      <Spacer y={0.5} />
+      <Text h3>Analise Data {analyseProgress > 99 &&
+      <CheckInCircleFill/>
+      } </Text>
+      <Progress type="success" value={analyseProgress} />
+
+      <Spacer y={3.5} />
 
       <div className={styles.graph}>
         <Doughnut data={data} width={50} height={50} />
       </div>
-
-      <Button type="success" auto>
-        Start analyzation
-      </Button>
-
-      <Spacer y={3.5} />
       <footer>Made with Heart by JGS.</footer>
     </div>
   );
